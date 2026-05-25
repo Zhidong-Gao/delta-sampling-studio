f00a26670fda861748a20de7db240da65fe99510
anon <anon@local>
2026-05-25 11:16:19 +0000

    Rotate tunnel URL to https://broadway-effects-leone-materials.trycloudflare.com (cache v=51)

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
  * ============================================================ */
 
 const CONFIG = {
-  COMFY_URL: "https://scuba-nicholas-html-measured.trycloudflare.com",
+  COMFY_URL: "https://broadway-effects-leone-materials.trycloudflare.com",
   hasBackend: false,
 };
 
