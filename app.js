e76674babd2794a44e893169c8a1bff4dd9f4010
anon <anon@local>
2026-05-26 10:48:32 +0000

    Rotate tunnel URL to https://continuing-significant-there-tucson.trycloudflare.com (cache v=63)

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
  * ============================================================ */
 
 const CONFIG = {
-  COMFY_URL: "https://submitting-door-shoulder-dont.trycloudflare.com",
+  COMFY_URL: "https://continuing-significant-there-tucson.trycloudflare.com",
   hasBackend: false,
 };
 
