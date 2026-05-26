413b04dcc02824335dc7c98e8a67a32f9cb6341a
anon <anon@local>
2026-05-26 10:26:28 +0000

    Rotate tunnel URL to https://submitting-door-shoulder-dont.trycloudflare.com (cache v=62)

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
  * ============================================================ */
 
 const CONFIG = {
-  COMFY_URL: "https://api.trycloudflare.com",
+  COMFY_URL: "https://submitting-door-shoulder-dont.trycloudflare.com",
   hasBackend: false,
 };
 
