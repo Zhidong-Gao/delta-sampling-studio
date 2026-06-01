8827054f62a6f5c05e7bdfb7fb5fbc4ff86eb946
anon <anon@local>
2026-06-01 05:04:15 +0000

    Rotate tunnel URL to https://had-granted-sky-wide.trycloudflare.com (cache v=84)

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
  * ============================================================ */
 
 const CONFIG = {
-  COMFY_URL: "https://motors-wondering-managing-translate.trycloudflare.com",
+  COMFY_URL: "https://had-granted-sky-wide.trycloudflare.com",
   hasBackend: false,
 };
 
