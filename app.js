d3fbe7331a583d2d6cb84f920c781e3e032a28e4
anon <anon@local>
2026-06-01 06:10:23 +0000

    Rotate tunnel URL to https://facial-intro-achieving-intelligent.trycloudflare.com (cache v=85)

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
  * ============================================================ */
 
 const CONFIG = {
-  COMFY_URL: "https://had-granted-sky-wide.trycloudflare.com",
+  COMFY_URL: "https://facial-intro-achieving-intelligent.trycloudflare.com",
   hasBackend: false,
 };
 
