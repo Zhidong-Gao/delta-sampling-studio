b0af0d34da11c514b5ab71980b037ae6b01886e2
anon <anon@local>
2026-06-03 13:41:59 +0000

    Rotate tunnel URL to https://wants-margin-information-publisher.trycloudflare.com (cache v=105)

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
  * ============================================================ */
 
 const CONFIG = {
-  COMFY_URL: "https://goto-active-skins-devoted.trycloudflare.com",
+  COMFY_URL: "https://wants-margin-information-publisher.trycloudflare.com",
   hasBackend: false,
 };
 
