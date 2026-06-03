2ce6383aeec8d601eb2a462a0f41ad57f3494b04
anon <anon@local>
2026-06-03 13:25:15 +0000

    Rotate tunnel URL to https://far-weight-really-hawk.trycloudflare.com (cache v=97)

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
  * ============================================================ */
 
 const CONFIG = {
-  COMFY_URL: "https://cents-reproduce-networking-estimation.trycloudflare.com",
+  COMFY_URL: "https://far-weight-really-hawk.trycloudflare.com",
   hasBackend: false,
 };
 
