93d8385017a4f1e6c351be1c17ec7190cdf5d469
anon <anon@local>
2026-07-16 17:19:18 +0000

    Rotate tunnel URL to https://reviewer-investigator-breeds-continuity.trycloudflare.com (cache v=150)

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
  * ============================================================ */
 
 const CONFIG = {
-  COMFY_URL: "https://rich-eds-configuring-untitled.trycloudflare.com",
+  COMFY_URL: "https://reviewer-investigator-breeds-continuity.trycloudflare.com",
   hasBackend: false,
 };
 
