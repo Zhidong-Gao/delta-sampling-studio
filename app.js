c80c17ee371808c225b7ac1bf9900bd0a46b9cef
anon <anon@local>
2026-07-16 15:47:14 +0000

    Rotate tunnel URL to https://known-garcia-rebel-tomato.trycloudflare.com (cache v=138)

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
  * ============================================================ */
 
 const CONFIG = {
-  COMFY_URL: "https://mozilla-triumph-berkeley-happening.trycloudflare.com",
+  COMFY_URL: "https://known-garcia-rebel-tomato.trycloudflare.com",
   hasBackend: false,
 };
 
