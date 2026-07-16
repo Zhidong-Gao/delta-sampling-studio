8072ce847a603938c28f163662e36cce50a16fe9
anon <anon@local>
2026-07-16 17:41:06 +0000

    Rotate tunnel URL to https://kentucky-reid-christine-public.trycloudflare.com (cache v=161)

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
  * ============================================================ */
 
 const CONFIG = {
-  COMFY_URL: "https://improve-father-throughout-tire.trycloudflare.com",
+  COMFY_URL: "https://kentucky-reid-christine-public.trycloudflare.com",
   hasBackend: false,
 };
 
