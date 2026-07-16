aeb04c01a34e6405cc629cb732678d0dddcc5866
anon <anon@local>
2026-07-16 15:07:22 +0000

    Rotate tunnel URL to https://optimum-wyoming-pub-opportunity.trycloudflare.com (cache v=120)

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
  * ============================================================ */
 
 const CONFIG = {
-  COMFY_URL: "https://necessarily-assist-dig-reward.trycloudflare.com",
+  COMFY_URL: "https://optimum-wyoming-pub-opportunity.trycloudflare.com",
   hasBackend: false,
 };
 
