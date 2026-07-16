3eb07975c42761afdc17851a74e6e2a381e5bddb
anon <anon@local>
2026-07-16 17:21:05 +0000

    Rotate tunnel URL to https://reaction-latter-such-modes.trycloudflare.com (cache v=151)

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
  * ============================================================ */
 
 const CONFIG = {
-  COMFY_URL: "https://reviewer-investigator-breeds-continuity.trycloudflare.com",
+  COMFY_URL: "https://reaction-latter-such-modes.trycloudflare.com",
   hasBackend: false,
 };
 
