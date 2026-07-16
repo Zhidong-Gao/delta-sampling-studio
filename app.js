b48f58fc8b6924a35b8e5c083e75c3d6c16f37f3
anon <anon@local>
2026-07-16 21:57:03 +0000

    Rotate tunnel URL to https://cabinet-fioricet-jay-electro.trycloudflare.com (cache v=172)

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
  * ============================================================ */
 
 const CONFIG = {
-  COMFY_URL: "https://coins-forty-batman-fashion.trycloudflare.com",
+  COMFY_URL: "https://cabinet-fioricet-jay-electro.trycloudflare.com",
   hasBackend: false,
 };
 
