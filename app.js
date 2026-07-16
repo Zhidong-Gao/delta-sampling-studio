d9acc24edcd90c6aa27125430cb8193520b03683
anon <anon@local>
2026-07-16 14:55:57 +0000

    Rotate tunnel URL to https://yard-extensions-browse-gazette.trycloudflare.com (cache v=115)

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
  * ============================================================ */
 
 const CONFIG = {
-  COMFY_URL: "https://big-wma-volumes-correct.trycloudflare.com",
+  COMFY_URL: "https://yard-extensions-browse-gazette.trycloudflare.com",
   hasBackend: false,
 };
 
